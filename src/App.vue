bb249b667cb0feaed5feaad2abf3c9c83029a0bf
Le Phu Hai <lephuhai0511@gmail.com>
2016-12-21 13:55:43 +0700

    Commit vue-starter-kit structure

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <aboutme></aboutme>
+  </div>
+</template>
+
+<script>
+import Aboutme from './components/Aboutme'
+
+export default {
+  name: 'app',
+  components: {
+    Aboutme
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
